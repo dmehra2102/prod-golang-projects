5e3609226fd5c777f1306708d831632f39c707eb
dmehra2102 <deepanshumehra2102@gmail.com>
2026-02-23 03:01:28 +0000

    feat: added common error types

---
+++ medflow/errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import "errors"
+
+var (
+	ErrPatientNotFound      = errors.New("patient not found")
+	ErrPatientAlreadyExists = errors.New("patient with this national ID already exists")
+	ErrPatientDeceased      = errors.New("operation not permitted: patient is deceased")
+	ErrInvalidGender        = errors.New("invalid gender value")
+	ErrInvalidBloodType     = errors.New("invalid blood type")
+	ErrInvalidDateOfBirth   = errors.New("date of birth cannot be in the future")
+	ErrNationalIDRequired   = errors.New("national ID is required")
+)
